172f917fbafe0c968253eb67f485147cfc2e3534
ocki982 <maiderocta@gmail.com>
2022-05-16 16:51:58 -0700

    added routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import "./App.css"
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage"
+import LoginPage from "./pages/LoginPage/LoginPage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
 
 function App() {
 	return(
@@ -8,6 +11,9 @@ function App() {
 		<BrowserRouter>
 			<Switch>
 				<Route path="/home" component={HomePage}/>
+				<Route path="/login" component={LoginPage}/>
+				<Route path="/register" component={RegisterPage}/>
+				<Route path="/profile" component={ProfilePage}/>
 			</Switch>
 		</BrowserRouter>
 	</div>
