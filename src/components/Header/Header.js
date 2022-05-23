4b0ed60f0d0de42a12f4e53e3522e2f9a8a326e4
ocki982 <maiderocta@gmail.com>
2022-05-23 16:13:07 -0700

    styled header bar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Header.scss'
+import './Header.scss';
+import Avatar from '../../assets/images/user.png';
+import Help from '../../assets/images/question_icon.svg';
 
 const Header = () => {
     return (
         <div className='header'>
-            <Link to="/home">
+            <Link className='header__link' to="/home">
                 <h1 className='header__title'>Color me up!</h1>
             </Link>
-            <Link to="/profile">
-                <img className='header__avatar' alt="avatar"/>
+            <div>
+            <img className='header__help' alt='help' src={Help}/>
+            <Link className='header__link' to="/profile">
+                <img className='header__avatar' alt="avatar" src={Avatar}/>
             </Link>
+            </div>
         </div>
     );
 };
